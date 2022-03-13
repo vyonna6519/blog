fc2dada4d4a97227928d5b8e38ab5853ff0b29f7
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-13 22:15:41 +0300

    updated classes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, SubmitField, 
                     ValidationError, BooleanField)
-from wtforms.validators import Required, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 
 class SignUpForm(FlaskForm):
-    first_name = StringField("Your First Name", validators=[Required()])
-    last_name = StringField("Your Last Name", validators=[Required()])
-    username = StringField("Your Username", validators=[Required()])
-    email = StringField("Your Email Address", validators=[Required(), Email()])
-    password = PasswordField("Password", validators=[Required(), 
+    first_name = StringField("Your First Name", validators=[DataRequired()])
+    last_name = StringField("Your Last Name", validators=[DataRequired()])
+    username = StringField("Your Username", validators=[DataRequired()])
+    email = StringField("Your Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), 
                              EqualTo("password_confirm", message = "Passwords must match")])
-    password_confirm = PasswordField("Confirm Password", validators=[Required()])
+    password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Sign Up")
 
     #Custom email validation
@@ -25,7 +25,7 @@ class SignUpForm(FlaskForm):
             raise ValidationError("That username is taken")
 
 class LoginForm(FlaskForm):
-    email = StringField("Your Email Address", validators=[Required(), Email()])
-    password = PasswordField("Password", validators=[Required()])
+    email = StringField("Your Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
