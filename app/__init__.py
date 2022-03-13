a196f219347b2a0db4cfd7dce44c3b4df4b03067
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-13 18:28:45 +0300

    initialized create-app and added imports

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
 
     # Initializing flask extensions
     db.init_app(app)
