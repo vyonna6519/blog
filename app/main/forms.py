f2ad794bbda727a23eef3f1ce924c905bdf63c06
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-13 22:14:05 +0300

    updated classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,20 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField,
                     SubmitField, SelectField)
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
-    title = StringField("Blog title:", validators=[Required()])
-    post = TextAreaField("Type Away:", validators=[Required()])
+    title = StringField("Blog title:", validators=[DataRequired()])
+    post = TextAreaField("Type Away:", validators=[DataRequired()])
     submit = SubmitField("Post")
 
 class UpdatePostForm(FlaskForm):
-    title = StringField("Blog title", validators=[Required()])
-    post = TextAreaField("Type Away", validators=[Required()])
+    title = StringField("Blog title", validators=[DataRequired()])
+    post = TextAreaField("Type Away", validators=[DataRequired()])
     submit = SubmitField("Update")
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField("Post Comment", validators=[Required()])
+    comment = TextAreaField("Post Comment", validators=[DataRequired()])
     alias = StringField("Comment Alias")
     submit = SubmitField("Comment")
 
