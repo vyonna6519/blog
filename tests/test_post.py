29b550bef1d7f7eae96db98ef66f9e7f1f84718a
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-13 17:07:10 +0300

    added unnitest for class testpost

--- tests/test_post.py
+++ tests/test_post.py
@@ -4,19 +4,19 @@ from app.models import Post, User, Comment
 class TestPost(unittest.TestCase):
     
     def setUp(self):
-        self.user_Victor = User(first_name = "Victor",
-                                last_name = "Kibocha",
-                                username = "Rocker",
+        self.user_Vyonna = User(first_name = "Vyonna",
+                                last_name = "Njenga",
+                                username = "Vee",
                                 password = "0719890523",
-                                email = "vicikibocha@gmail.com")
+                                email = "njengavyonna@gmail.com")
         self.new_post = Post(post_title = "Sample Title",
-                            post_content = "The jig is up! Every time I meet new people I introduce myself as 'I sell dope'.",
-                            user_id = self.user_Victor.id)
+                            post_content = "Every time I meet new people I introduce myself as 'I sell dope'.",
+                            user_id = self.user_Vyonna.id)
         self.new_comment = Comment(comment = "Nice job! Love it.",
                                     post_id = self.new_post.id,
-                                    user_id = self.user_Victor.id)
+                                    user_id = self.user_Vyonna.id)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.user_Victor, User))
+        self.assertTrue(isinstance(self.user_Vyonna, User))
         self.assertTrue(isinstance(self.new_post, Post))
         self.assertTrue(isinstance(self.new_comment, Comment))
